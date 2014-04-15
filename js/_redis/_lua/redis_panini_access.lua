fae72e1e81fc5450171ce404f873c47360486d5f
Brian C Sparks <brian.c.sparks@hp.com>
2014-04-15 03:30:37 +0000

    Ignore some entries

---
+++ js/_redis/_lua/redis_panini_access.lua
@@ -0,0 +1,192 @@
+
+local s_current_second  = skey('current_second', typename)
+local s_model_projects  = skey('model', 'projects')
+local s_method_post     = skey('method', 'post')
+
+local s_cs_post_project = skey('current_second', 'project-post')
+local s_post_project    = skey(typename, 'project-post')
+local s_cs_onramp       = skey('current_second', 'onramp')
+local s_onramp          = skey(typename, 'onramp')
+local s_cs_checkout     = skey('current_second', 'checkout')
+local s_checkout        = skey(typename, 'checkout')
+
+local s_has_onrampA     = skey(typename, 'has_onrampA')
+local s_has_onramp      = skey(typename, 'has_onramp')
+
+local s_lone_pproject   = skey(typename, 'lone_pproject')
+
+local s_has_time_to_pproject      = skey(typename, 'has_time_to_pproject')
+local s_has_subsequent_pprojectA  = skey(typename, 'has_subsequent_pprojectA')
+local s_has_subsequent_pproject   = skey(typename, 'has_subsequent_pproject')
+
+local s_has_onramp_ip_id          = skey(typename, 'has_onramp_ip_id')
+
+local function init()
+end
+
+local function __user_commit_slug(...)
+  local method   = arg[6]
+  local action   = arg[15]
+  local pathname = arg[18]
+
+  if method == 'post' and pathname == '/projects' then
+    redis.call('SADD', s_cs_post_project, curr_slug)
+    curr_slug_expiration = curr_slug_date
+  elseif pathname == '/arts' or pathname == '/merchants' or pathname == '/' then
+    redis.call('SADD', s_cs_onramp, curr_slug)
+    curr_slug_expiration = curr_slug_date
+  elseif action == 'checkout_success' then
+    redis.call('SADD', s_cs_checkout, curr_slug)
+    curr_slug_expiration = curr_slug_date
+  elseif enum_type == 'serialize' then
+    abort_commit = 1
+  end
+
+  if abort_commit ~= 1 then
+    __commit_slug(unpack(arg))
+  end
+end
+
+local function closeSecond(s_, prev_s)
+
+  local s = string.match(s_, '(%d%d)$')
+
+  -- Loop over the onramp slugs, and remember the associated IPs
+  local onramp, ip_ids = slugmembers_plus1(s_cs_onramp, 'ip_id')
+  if #onramp > 0 then
+    local ba = {}
+    for i, slug in pairs(onramp) do
+      local ip_id = ip_ids[i]
+
+      redis.call('SETNX', skey('onramp_ip_id', ip_id), slug)
+      ba = bulk_sadd(ba, 200, s_has_onramp_ip_id, skey('onramp_ip_id', ip_id))
+    end
+
+    bulk_sadd(ba, 0, s_has_onramp_ip_id)
+
+  end
+
+  -- Move the slugs out of the way for the next second, but just rename them to be quick
+  redis.call('RENAME', s_cs_onramp, 'second:'..s..':'..typename..'_onramp_slug_')
+
+  -- Loop over the POST /projects and find the onramp slugs via the IP address that
+  -- they both share
+  local pprojects, ip_ids = slugmembers_plus1(s_cs_post_project, 'ip_id')
+  if #pprojects > 0 then
+    local bb = {}
+    local bc = {}
+    local bd = {}
+    local be = {}
+    local bf = {}
+    local bg = {}
+
+    for i, slug_ in pairs(pprojects) do
+      local ip_id = ip_ids[i]
+      local slug = tonumber(slug_)
+
+      -- Is there a slug for this IP entering on an onramp?
+      if redis.call('EXISTS', skey('onramp_ip_id', ip_id)) == 1 then
+        local onramp_slug = tonumber(redis.call('GET', skey('onramp_ip_id', ip_id)))
+
+        -- Make sure the onramp happened before the POST /project
+        if onramp_slug < slug then
+
+          bb = bulk_set(bb, 200, slugkey2(slug, 'onramp'), onramp_slug)
+          bd = bulk_del(bd, 200, skey('onramp_ip_id', ip_id))
+          be = bulk_sadd(be, 200, s_has_onrampA, slug)
+
+        else
+          bf = bulk_sadd(bf, 200, s_lone_pproject, slug)
+        end
+      else
+        -- No onramp, see if this is a re-posting of projects
+        if redis.call('EXISTS', skey('pprojects_ip_id', ip_id)) == 1 then
+          local orig_pprojects_slug = tonumber(redis.call('GET', skey('pprojects_ip_id', ip_id)))
+          redis.call('SADD', slugkey2(orig_pprojects_slug, 'subsequent_pproject'), slug)
+
+          bc = bulk_set(bc, 200, slugkey2(slug, 'orig_pproject'), orig_pprojects_slug)
+          bg = bulk_sadd(bg, 200, s_has_subsequent_pproject, orig_pprojects_slug)
+        else
+          bf = bulk_sadd(bf, 200, s_lone_pproject, slug)
+        end
+      end
+
+      redis.call('SETNX', skey('pprojects_ip_id', ip_id), slug)
+    end
+
+    bulk_set(bb, 0)
+    bulk_set(bc, 0)
+    bulk_del(bd, 0)
+    bulk_sadd(be, 0, s_has_onrampA)
+    bulk_sadd(bf, 0, s_lone_pproject)
+    bulk_sadd(bg, 0, s_has_subsequent_pproject)
+  end
+
+  spromote(s_cs_post_project, s_post_project)
+end
+
+local function closeMinute(a, b)
+
+  --redis.log(redis.LOG_NOTICE, 'userCloseMinute '..b)
+
+  -- These were moved out of the way in closeSecond.  Now, put them onto permanent storage, and delete them below
+  --redis.pcall('SUNIONSTORE', s_onramp, s_onramp,
+  --  'second:00:'..typename..'_onramp_slug_', 'second:01:'..typename..'_onramp_slug_', 'second:02:'..typename..'_onramp_slug_', 'second:03:'..typename..'_onramp_slug_', 'second:04:'..typename..'_onramp_slug_',
+  --  'second:05:'..typename..'_onramp_slug_', 'second:06:'..typename..'_onramp_slug_', 'second:07:'..typename..'_onramp_slug_', 'second:08:'..typename..'_onramp_slug_', 'second:09:'..typename..'_onramp_slug_',
+  --  'second:10:'..typename..'_onramp_slug_', 'second:11:'..typename..'_onramp_slug_', 'second:12:'..typename..'_onramp_slug_', 'second:13:'..typename..'_onramp_slug_', 'second:14:'..typename..'_onramp_slug_',
+  --  'second:15:'..typename..'_onramp_slug_', 'second:16:'..typename..'_onramp_slug_', 'second:17:'..typename..'_onramp_slug_', 'second:18:'..typename..'_onramp_slug_', 'second:19:'..typename..'_onramp_slug_',
+  --  'second:20:'..typename..'_onramp_slug_', 'second:21:'..typename..'_onramp_slug_', 'second:22:'..typename..'_onramp_slug_', 'second:23:'..typename..'_onramp_slug_', 'second:24:'..typename..'_onramp_slug_',
+  --  'second:25:'..typename..'_onramp_slug_', 'second:26:'..typename..'_onramp_slug_', 'second:27:'..typename..'_onramp_slug_', 'second:28:'..typename..'_onramp_slug_', 'second:29:'..typename..'_onramp_slug_',
+  --  'second:30:'..typename..'_onramp_slug_', 'second:31:'..typename..'_onramp_slug_', 'second:32:'..typename..'_onramp_slug_', 'second:33:'..typename..'_onramp_slug_', 'second:34:'..typename..'_onramp_slug_',
+  --  'second:35:'..typename..'_onramp_slug_', 'second:36:'..typename..'_onramp_slug_', 'second:37:'..typename..'_onramp_slug_', 'second:38:'..typename..'_onramp_slug_', 'second:39:'..typename..'_onramp_slug_',
+  --  'second:30:'..typename..'_onramp_slug_', 'second:41:'..typename..'_onramp_slug_', 'second:42:'..typename..'_onramp_slug_', 'second:43:'..typename..'_onramp_slug_', 'second:44:'..typename..'_onramp_slug_',
+  --  'second:45:'..typename..'_onramp_slug_', 'second:46:'..typename..'_onramp_slug_', 'second:47:'..typename..'_onramp_slug_', 'second:48:'..typename..'_onramp_slug_', 'second:49:'..typename..'_onramp_slug_',
+  --  'second:50:'..typename..'_onramp_slug_', 'second:51:'..typename..'_onramp_slug_', 'second:52:'..typename..'_onramp_slug_', 'second:53:'..typename..'_onramp_slug_', 'second:54:'..typename..'_onramp_slug_',
+  --  'second:55:'..typename..'_onramp_slug_', 'second:56:'..typename..'_onramp_slug_', 'second:57:'..typename..'_onramp_slug_', 'second:58:'..typename..'_onramp_slug_', 'second:59:'..typename..'_onramp_slug_'
+  --  )
+  redis.pcall('DEL', 
+    'second:00:'..typename..'_onramp_slug_', 'second:01:'..typename..'_onramp_slug_', 'second:02:'..typename..'_onramp_slug_', 'second:03:'..typename..'_onramp_slug_', 'second:04:'..typename..'_onramp_slug_',
+    'second:05:'..typename..'_onramp_slug_', 'second:06:'..typename..'_onramp_slug_', 'second:07:'..typename..'_onramp_slug_', 'second:08:'..typename..'_onramp_slug_', 'second:09:'..typename..'_onramp_slug_',
+    'second:10:'..typename..'_onramp_slug_', 'second:11:'..typename..'_onramp_slug_', 'second:12:'..typename..'_onramp_slug_', 'second:13:'..typename..'_onramp_slug_', 'second:14:'..typename..'_onramp_slug_',
+    'second:15:'..typename..'_onramp_slug_', 'second:16:'..typename..'_onramp_slug_', 'second:17:'..typename..'_onramp_slug_', 'second:18:'..typename..'_onramp_slug_', 'second:19:'..typename..'_onramp_slug_',
+    'second:20:'..typename..'_onramp_slug_', 'second:21:'..typename..'_onramp_slug_', 'second:22:'..typename..'_onramp_slug_', 'second:23:'..typename..'_onramp_slug_', 'second:24:'..typename..'_onramp_slug_',
+    'second:25:'..typename..'_onramp_slug_', 'second:26:'..typename..'_onramp_slug_', 'second:27:'..typename..'_onramp_slug_', 'second:28:'..typename..'_onramp_slug_', 'second:29:'..typename..'_onramp_slug_',
+    'second:30:'..typename..'_onramp_slug_', 'second:31:'..typename..'_onramp_slug_', 'second:32:'..typename..'_onramp_slug_', 'second:33:'..typename..'_onramp_slug_', 'second:34:'..typename..'_onramp_slug_',
+    'second:35:'..typename..'_onramp_slug_', 'second:36:'..typename..'_onramp_slug_', 'second:37:'..typename..'_onramp_slug_', 'second:38:'..typename..'_onramp_slug_', 'second:39:'..typename..'_onramp_slug_',
+    'second:30:'..typename..'_onramp_slug_', 'second:41:'..typename..'_onramp_slug_', 'second:42:'..typename..'_onramp_slug_', 'second:43:'..typename..'_onramp_slug_', 'second:44:'..typename..'_onramp_slug_',
+    'second:45:'..typename..'_onramp_slug_', 'second:46:'..typename..'_onramp_slug_', 'second:47:'..typename..'_onramp_slug_', 'second:48:'..typename..'_onramp_slug_', 'second:49:'..typename..'_onramp_slug_',
+    'second:50:'..typename..'_onramp_slug_', 'second:51:'..typename..'_onramp_slug_', 'second:52:'..typename..'_onramp_slug_', 'second:53:'..typename..'_onramp_slug_', 'second:54:'..typename..'_onramp_slug_',
+    'second:55:'..typename..'_onramp_slug_', 'second:56:'..typename..'_onramp_slug_', 'second:57:'..typename..'_onramp_slug_', 'second:58:'..typename..'_onramp_slug_', 'second:59:'..typename..'_onramp_slug_'
+    )
+
+
+  -- Now, compute how long things took
+  local i, slug, start, stop, elapsed, onramp, aaa
+  local ba = {}
+  local bb = {}
+  local pprojectors = redis.call('SMEMBERS', s_has_onrampA)
+  table.sort(pprojectors)
+  if #pprojectors > 0 then
+    local onramps = convert_to(typename..'_slug', pprojectors, typename..'_onramp_slug_')
+
+    for i, slug in pairs(pprojectors) do
+      onramp = onramps[i]
+
+      aaa = redis.call('MGET', slugkey(onramp, 'n_time'), slugkey(slug, 'n_time'))
+      start, stop = aaa[1], aaa[2]
+      elapsed = tonumber(stop) - tonumber(start)
+      ba = bulk_set(ba, 200, slugkey(slug, 'time_to_pproject'), elapsed)
+      ba = bulk_set(ba, 200, slugkey(slug, 'ln_time_to_pproject'), math.floor(math.log(elapsed) * log2inv))
+      bb = bulk_sadd(bb, 200, s_has_time_to_pproject, slug)
+    end
+
+    bulk_set(ba, 0)
+    bulk_sadd(bb, 0, s_has_time_to_pproject)
+    spromote(s_has_onrampA, s_has_onramp)
+  end
+
+end
+
+local function closeDay(a, b)
+end
+
---
+++ js/redis_panini_access.js
@@ -0,0 +1,240 @@
+
+/**
+ *
+ */
+
+var urlLib = require('url');
+
+exports.init = function(context, callback) {
+
+  var _ = context._;
+
+  context.enumType = 'serialize';
+  //context.enumType = 'bulk';
+
+  var bulkTypes = [
+    /* 1: */ {name:'time', ty:'time'},
+    /* 2: */ {name:'server', ty:'lite_enum'},
+    /* 3: */ {name:'ip', ty:'ip_addr'},
+    /* 4: */ {name:'elapsed', ty:'ln'},
+    /* 5: */ {},
+    /* 6: */ {name:'method', ty:'lite_enum'},
+    /* 7: */ {name:'url', ty:'string'},
+    /* 8: */ {name:'http_ver', ty:'lite_enum'},
+    /* 9: */ {name:'resp_code', ty:'nenum'},
+    /* 10: */ {name:'size', ty:'ln'},
+    /* 11: */ {name:'referer', ty:'string'},
+    /* 12: */ {name:'user_agent', ty:'string'},
+
+    /* 13: */ {name:'unknown', ty:'string'},
+    /* 14: */ {name:'model', ty:'lite_enum'},
+    /* 15: */ {name:'action', ty:'lite_enum'},
+    /* 16: */ {name:'obj_id', ty:'db_id'},
+    /* 17: */ {name:'obj_guid', ty:'guid'},
+    /* 18: */ {name:'pathname', ty:'lite_enum'},
+    /* 19: */ {name:'page', ty:'lite_enum'}
+  ];
+
+  var serializeTypes = [
+    /* 1: */ {name:'time', ty:'time'},
+    /* 2: */ {},   /*'server:lite_enum',*/
+    /* 3: */ {name:'ip', ty:'ip_addr'},
+    /* 4: */ {}, /*'elapsed:ln',*/
+    /* 5: */ {},
+    /* 6: */ {name:'method', ty:'lite_enum'},
+    /* 7: */ {}, /*'url:string',*/
+    /* 8: */ {}, /*'http_ver:lite_enum',*/
+    /* 9: */ {name:'resp_code', ty:'nenum'},
+    /* 10: */ {}, /*'size:ln',*/
+    /* 11: */ {}, /*'referer:string',*/
+    /* 12: */ {}, /*'user_agent:string',*/
+
+    /* 13: */ {name:'unknown', ty:'string'},
+    /* 14: */ {name:'model', ty:'lite_enum'},
+    /* 15: */ {name:'action', ty:'lite_enum'},
+    /* 16: */ {name:'obj_id', ty:'db_id'},
+    /* 17: */ {name:'obj_guid', ty:'guid'},
+    /* 18: */ {name:'pathname', ty:'lite_enum'},
+    /* 19: */ {name:'page', ty:'lite_enum'}
+  ];
+
+  context.typeName = 'access';
+
+  if (context.enumType === 'bulk') {
+    context.types = bulkTypes;
+  } else {
+    context.types = serializeTypes;
+  }
+
+  var pageNames = {
+    '/'           : true,
+    '/arts'       : true,
+    '/merchants'  : true
+  };
+
+  var modelNames_wId = {
+    merchants   : true,
+    orders      : true,
+    arts        : true
+  };
+
+  var modelNames_wGuid = {
+    orders      : true,
+    projects    : true
+  };
+
+  var modelNames = _.extend({}, modelNames_wId, modelNames_wGuid);
+
+  context.split = function(line) {
+    var ret = line.split(/[ \t]/);
+    if (ret.length > 12) { return 'ENOPARSE'; }
+    if (ret.length < 11) {
+      if (ret.length === 10 && /\d+/.exec(ret[3]) && /\d\d\d/.exec(ret[6]) && /\d+/.exec(ret[7])) {
+        ret.splice(5, 0, '-', '-');
+      } else {
+        return 'ENOPARSE';
+      }
+    }
+
+    if (!/mozilla/i.exec(ret[11])) {
+      return null;
+    }
+
+    ret.unshift(line);
+
+    ret.raw = ret.slice();
+    ret.raw[0] = '-';
+    ret.time = ret[1].split(/[^0-9]/);
+
+    ret[2] = '-';
+    ret[8] = '-';
+    ret[12] = '-';
+
+    if (context.enumType !== 'bulk') {
+      ret[7] = '-';
+      ret[11] = '-';
+    }
+
+    return ret;
+  };
+
+  var g_fields;
+  var parseUrl = function(pathname, url, parts) {
+    url = url || urlLib.parse(pathname, true);
+    if (url.pathname) {
+      if (!parts) {
+        parts = url.pathname.split('/');
+        parts.shift();
+      }
+    } else {
+      console.log('error parsing url: ' + pathname);
+      console.log(g_fields);
+    }
+
+    return {url:url, parts:parts};
+  };
+
+  context.processFields = function(fields, options, callback) {
+    g_fields = fields;
+
+    fields[13] = fields[14] = fields[15] = fields[16] = fields[17] = fields[18] = fields[19] = '-';
+
+    var pathname = fields.raw[7];
+    var url, parts, urlUnderstood = false;
+    //console.log(pathname);
+
+    if (/^\/generatetextlayer/i.exec(pathname)) {
+      return callback(null, null);
+    }
+
+    if (!options.skip.pathname) {
+      url   = url   || parseUrl(pathname, url, parts).url;
+      fields[18] = url.pathname;
+    }
+
+    if (!options.skip.model) {
+      url   = url   || parseUrl(pathname, url, parts).url;
+      parts = parts || parseUrl(pathname, url, parts).parts;
+
+      if (modelNames[parts[0]]) {
+        fields[14] = parts[0];
+        urlUnderstood = true;
+      } 
+    }
+
+    if (!options.skip.page) {
+      url   = url   || parseUrl(pathname, url, parts).url;
+      parts = parts || parseUrl(pathname, url, parts).parts;
+
+      if (pageNames['/'+parts[0]]) {
+        fields[19] = parts[0];
+        urlUnderstood = true;
+      } 
+    }
+
+    if (!options.skip.action) {
+      url   = url   || parseUrl(pathname, url, parts).url;
+      parts = parts || parseUrl(pathname, url, parts).parts;
+
+      if (modelNames[parts[0]]) {
+        if (parts.length === 2) {
+          fields[15] = parts[1];
+        } else if (parts.length > 2) {
+          fields[15] = parts[2];
+        }
+      }
+    }
+
+    if (!options.skip.model_id) {
+      url   = url   || parseUrl(pathname, url, parts).url;
+      parts = parts || parseUrl(pathname, url, parts).parts;
+
+      if (parts.length > 2 && modelNames_wId[parts[0]]) {
+        fields[16] = parts[1];
+      }
+    }
+
+    if (!options.skip.model_guid) {
+      url   = url   || parseUrl(pathname, url, parts).url;
+      parts = parts || parseUrl(pathname, url, parts).parts;
+
+      if (parts.length > 2 && modelNames_wGuid[parts[0]]) {
+        fields[17] = parts[1];
+      }
+    }
+
+    if (!options.skip.unknown) {
+      url   = url   || parseUrl(pathname, url, parts).url;
+
+      if (!urlUnderstood && context.enumType === 'bulk') {
+        fields[13] = url.pathname;
+      }
+    }
+
+    return callback(null, fields);
+  };
+
+  context.on('closeSecond', function(a, b, callback) {
+    //if (context.enumType === 'bulk') { return callback(a, b); }
+    context.closeSecond(a, b);
+
+    return callback(a, b);
+  });
+
+  context.on('closeMinute', function(a, b, callback) {
+    //if (context.enumType === 'bulk') { return callback(a, b); }
+    context.closeMinute(a, b);
+
+    return callback(a, b);
+  });
+
+  context.on('closeDay', function(a, b, callback) {
+    //if (context.enumType === 'bulk') { return callback(a, b); }
+    context.closeDay(a, b);
+
+    return callback(a, b);
+  });
+
+  return callback();
+};
+
