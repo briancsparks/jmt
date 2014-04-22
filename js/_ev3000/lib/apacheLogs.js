575f5ba93a8ea8e67985b44ee9d0cfb8d5ca32c9
Brian C Sparks <brian.c.sparks@hp.com>
2014-04-22 08:42:47 -0700

    WIP

--- js/_ev3000/lib/apacheLogs.js
+++ js/_ev3000/lib/apacheLogs.js
@@ -1,30 +1,33 @@
 
-var maxField = d.std.strings.length - 1;
-d.std.rawList.process = function(rawList, options, callback) {
-  var list = _.map(rawList, function(record) {
-    //return record.split(/[ \t]+/);
+(function() {
+  var maxField = d.std.strings.length - 1;
+  d.std.rawList.process = function(rawList, options, callback) {
+    var list = _.map(rawList, function(record) {
+      //return record.split(/[ \t]+/);
 
-    var fields = _.map(record.split(/[ \t]+/), function(field, index) {
-      if (/^[0-9]+/.exec(field)) { return parseInt(field, 10); }
-      if (index > maxField) { index = maxField;}
+      var fields = _.map(record.split(/[ \t]+/), function(field, index) {
+        if (/^[0-9]+/.exec(field)) { return parseInt(field, 10); }
+        if (index > maxField) { index = maxField;}
 
-      if (index === 11) {
-        field = '-';
-      }
-      else if (index === 6) {
-        field = _.first(field.split('#', 1)[0].split('?', 1)[0].split('/'), 2).join('/');
-      }
-      else if (index === 10) {
-        field = _.first(field.split('#', 1)[0].split('?', 1)[0].split('/'), 4).join('/');
-      }
+        if (index === 11) {
+          field = '-';
+        }
+        else if (index === 6) {
+          field = _.first(field.split('#', 1)[0].split('?', 1)[0].split('/'), 2).join('/');
+        }
+        else if (index === 10) {
+          field = _.first(field.split('#', 1)[0].split('?', 1)[0].split('/'), 4).join('/');
+        }
 
-      d.std.strings[index][field] = (d.std.strings[index][field] || field);
-      return d.std.strings[index][field];
+        d.std.strings[index][field] = (d.std.strings[index][field] || field);
+        return d.std.strings[index][field];
+      });
+
+      return fields;
     });
 
-    return fields;
-  });
-
-  return callback(null, list, options);
-};
+    return callback(null, list, options);
+  };
+  return 'Loaded apacheLogs processor';
+}());
 
