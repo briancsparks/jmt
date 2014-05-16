6215fc3cd7411dbdc419823a083f3558ff70e9c0
Brian C Sparks <brian.c.sparks@hp.com>
2014-05-16 22:41:33 +0000

    Good memory management

--- js/_v16/lib/apacheLog.js
+++ js/_v16/lib/apacheLog.js
@@ -1,71 +1,183 @@
+/**
+ *  @fileOverview: Do a first-pass optimization of the data, assuming
+ *  it is apache log file formatted;  Provide helper functions for 
+ *  dealing with apache formatted log entries.
+ */
 
 (function() {
+  // We want to run async
   var callback = async();
-  var result = {};
-  var dash = '-';
-  var dict = {}, queryDict = {};
 
-  dict['paths'] = dict['paths'] || {};
+  var result = {};                          // The result of processing
+  var dash = '-';                           // A single copy of this much-used string
+  var dict = {}, queryDict = {};            // Objects to hold duplicate strings
+
+  dict['paths'] = dict['paths'] || {};      // An object to hold duplicate strings in URL paths
 
   /**
-   *  A main function so the real meat of the module is here at the top of the file, and
-   *  helpers are below.
+   *  The main() function.
+   *
+   *  The real meat of the module is here at the top of the file, and helpers are below.
    */
   var main = function() {
 
     sg.__run([
 
-      // ----- Save some space -----
+      /*
+       *  Remove records that have little valuable information
+       *
+       *  * Mal-formed (NF < 10)
+       *  * Infrastructure-probing (408)
+       *  * Monitors / spiders
+       */
       function(next) {
-        //return next();    // skip for now
+        //return next();
+        var remove;               // Declare here, so we don't create and destroy it inside the main loop
 
-        result.seen    = 0;
-        result.removed = 0;
+        result.seen     = 0;      // How many records did we see on the first pass?
+        result.removed  = 0;      // How many were removed by this function?
+        result.noisy    = {};     // Histogram of noisy records
 
-        var remove;
-
+        // Loop over each record...
         eachRecord(function(record, index, column) {
             remove = false;
-
-            if (record.length < 10) { remove = true; }
-            if (record[6] == 408)   { remove = true; }
-
-            if (/^(Apache|ELB|NewRelic)/.exec(record[11])) { remove = true; }
-
-            if (/^.(assets|generatetextlayer)/i.exec(record[6]))                { remove = true; }
-
             result.seen++;
+
+            // ...determine if we want to delete it...
+            if (record.length < 10)                               { remove = 'tooSmall'; }
+            else if (record[6] == 408)                            { remove = '408'; }
+            else if (/^(Apache|ELB|NewRelic)/.exec(record[11]))   { remove = 'monitor'; }
+
+            // ... if so, remove it
             if (remove) {
               result.removed++;
+              result.noisy[remove] = (result.noisy[remove] || 0) + 1;
               delete column[index];
+
+            // ... if not, clobber low-value individual fields
             } else {
               // server name, http version, user agent
               column[index][1] = column[index][7] = column[index][11] = dash;
             }
           }, 
 
+          // After each column, run the garbage collector
           function() {
             global.gc();
           }
         );
 
+        console.log('Pruned ' + result.removed + ' of ' + result.seen + ' total records (' + Math.floor(100 * result.removed/result.seen) + '%).');
+        var msg = _.map(result.noisy, function(count, key) {
+          return '\t' + key + ': ' + count;
+        });
+        console.log(msg.join('\n'));
+
+        // We've removed many records; compact the data set
         compactData();
         return next();
       },
 
-      // ----- Convert URL and referer into objects -----
+      /*
+       *  Hide records that may not need to be fully parsed.
+       *
+       *  * Assets and text-layer
+       */
       function(next) {
-        //return next();    // skip for now
+        //return next();
+        var hide;               // Declare here, so we don't create and destroy it inside the main loop
 
-        result.urlSeen    = 0;
-        result.nfHist  = {};
+        result.hidden   = 0;      // How many were hidden by this function?
+        result.hNoisey   = {};     // Histogram of noisy records
 
+        // Loop over each record...
+        eachRecord(function(record, index, column) {
+            hide = false;
+            result.seen++;
+
+            // ...determine if we want to hide it...
+            if (/^.assets/i.exec(record[6]))                      { hide = 'assets'; }
+            else if (/^.generatetextlayer/i.exec(record[6]))      { hide = 'gtl'; }
+
+            // ... if so, hide it
+            if (hide) {
+              result.hidden++;
+              result.hNoisey[hide] = (result.hNoisey[hide] || 0) + 1;
+
+              // This line changes the field from a string to an object.  This keeps other 
+              // functions from working on it
+              column[index][6]  = {just: record[6]};
+              column[index][10] = {just: record[10]};
+            }
+          }, 
+
+          // After each column, run the garbage collector
+          function() {
+            global.gc();
+          }
+        );
+
+        console.log('Hid ' + result.hidden + ' of ' + result.seen + ' total records (' + Math.floor(100 * result.hidden/result.seen) + '%).');
+        var msg = _.map(result.hNoisey, function(count, key) {
+          return '\t' + key + ': ' + count;
+        });
+        console.log(msg.join('\n'));
+
+        return next();
+      },
+      
+      /*
+       *  Remove records that are valid, but are way too plentiful
+       */
+      function(next) {
+        //return next();    // Skip for now
+
+        var url;
+
+        result.numNoisyRemoved = 0;
+
+        eachRecord(
+          function(record, index, column) {
+            url = record[6];
+
+            // If the URL has been hidden:
+            if (url.hasOwnProperty('just')) {
+              if (/^.(assets|generatetextlayer)/i.exec(url.just)) {
+                delete column[index];
+                result.numNoisyRemoved++;
+              }
+            }
+          },
+          function() {
+            global.gc();
+          }
+        );
+        compactData();
+        global.gc();
+
+        console.log('Removed ' + result.numNoisyRemoved + ' noisy records');
+        return next();
+      },
+
+      /*
+       * Convert the URL and referer fields into objects
+       *
+       * * Make sure we are parsing a string (it may have already been parsed
+       * * Try and minimize memory requirements
+       */
+      function(next) {
+        //return next();
+
+        result.urlSeen = 0;         // Remember how many URLs we've seen and processed
+        result.nfHist  = {};        // Make a histogram of the number of fields
+
+        // Loop over each record...
         eachRecord(
           function(record, index, column) {
             result.urlSeen++;
             result.nfHist[record.length] = (result.nfHist[record.length] || 0) + 1;
 
-            // ----- Process the URL field -----
+            // ... process the URL field...
             if (record.length < 7) { return; }
 
             url = record[6];
@@ -75,9 +187,10 @@
               reportBadRecord(record, 6);
             }
 
-            // ----- Process the referer field -----
+            // ...process the referer field...
             if (record.length < 11) { return; }
 
+            // ... and minify the resultant object
             url = record[10];
             if (typeof url === 'string' && url.length > 0) {
               record[10] = minifyUrlObject(url);
@@ -86,70 +199,60 @@
             }
           }, 
 
+          // On each column, run garbage collection
           function() {
             global.gc();
           }
         );
 
+        console.log('Converted ' + result.urlSeen + ' URLS');
         return next();
       },
       
+      /*
+       *  Remove records that are valid, but are way too plentiful
+       */
       function(next) {
+        return next();    // Skip for now
+
         var url;
+
+        result.numNoisyRemoved = 0;
 
         eachRecord(
           function(record, index, column) {
             url = record[6];
+
+            // If the URL has been parsed:
             if (url.hasOwnProperty('paths') && url.paths[0] === 'assets') {
               delete column[index];
+              result.numNoisyRemoved++;
             }
           },
           function() {
-            compactData();
             global.gc();
           }
         );
+        compactData();
+        global.gc();
 
+        console.log('Removed ' + result.numNoisyRemoved + ' asset records');
         return next();
       }],
 
+      /*
+       *  Done!!
+       *
+       *  Return the result
+       */
       function done() {
         return callback(null, result);
       }
     );
   };
 
-  var eachRecord = function(fn, fnAfterColumn_) {
-    var fnAfterColumn = fnAfterColumn_ || function() {};
-
-    // The inner loops get run bazillions of times, don't re-allocate variables.
-    var i, j, l, column, record;
-
-    // Loop over the columns (i), and the records (j)
-    for (i = 0; i < d.length; i++) {
-      column = d[i];
-      for (j = 0, l = column.length; j < l; j++) {
-        if (column.hasOwnProperty(j)) {
-          record = column[j];
-          fn(record, j, column, i);
-        }
-      }
-
-      fnAfterColumn(i, column);
-    }
-  };
-
-  var reportBadRecord = function(record, index) {
-    var url = record[index];
-    if (typeof url === 'number' && url >= 0 && url <= 999 && record.length === 10) {
-      // This is OK
-    } else {
-      console.error('Is this a url? ' + url + ' NF: ' + record.length + ' (' + record.join(' ') + ')');
-    }
-  };
-
   /**
-   *  A function to make the URL much faster to query (make it an object, not a string), while still
+   *  This function makes the URL much faster to query (make it an object, not a string), while still
    *  using minimal space:
    *
    *  * Any property that is null, undefined, or a zero-length string is deleted.
@@ -212,6 +315,22 @@
     }
 
     return url;
+  };
+
+  /**
+   *  When an above function finds a URL that is bad, it skips it, but
+   *  also calls this function to report it.  This function knows that
+   *  some URLs / records are just weird, so it suppresses the warning for those.
+   */
+  var reportBadRecord = function(record, index) {
+    var url = record[index];
+
+    // These are OK
+    if (_.isObject(url)) { return; }
+    if (typeof url === 'number' && url >= 0 && url <= 999 && record.length === 10) { return; }
+
+    /* Hmmmmmm... */
+    console.error('Is this a url? ' + url + ' NF: ' + record.length + ' (' + record.join(' ') + ')');
   };
 
   main();
--- js/_v16/lib/multiChild.js
+++ js/_v16/lib/multiChild.js
@@ -1,15 +1,29 @@
 /**
- *  @fileOverview: Setup for the child processes
+ *  @fileOverview: Functionality to manage a data set that is part of a
+ *  much larger set of data.
+ *
+ *  Generally, these are functions to help query and manipulate this data set,
+ *  but there are a few functions (like ingest, and its cousins) that handle
+ *  insertion of the data.
  */
 
 (function() {
-  init('raw', []);
-  init('d',   [[]]);
+  init('raw',   []);
+  init('d',     [[]]);
+  init('index', {});
 
   var dicts = _.map(_.range(30), function() { return {}; });
 
-  var tooManyRaw = 10000, tooManyData = 50000, wayTooManyData = 350000;
+  var tooManyRaw = 20000, tooManyData = 50000, wayTooManyData = 350000;
 
+  // Weird, but this randomization slows down processing
+  //tooManyRaw = Math.floor(tooManyRaw * (Math.random() + 1));
+
+  /**
+   *  ingest() -- Put the lines (records) into a temporary variable (raw) so
+   *  I can quickly return, then kick off a function to move it from the raw
+   *  area to the real data area (d).
+   */
   var forcedProcessRaw;
   ingest = function(lines, callback) {
     raw = raw.concat(lines);
@@ -28,6 +42,14 @@
     return callback(null, raw.length);
   };
 
+  /**
+   *  Process the raw records.
+   *
+   *  1. Break the line apart on whitespace (into an array)
+   *  2. Convert any numbers to the Number type
+   *  3. Use a dictionary to have one string that is referenced by many objects.
+   *  4. Run the garbage collector at the end.
+   */
   processRaw = function(force) {
     var item;
 
@@ -60,11 +82,17 @@
     }
   };
 
+  /**
+   *  Should the field use the dictionary technique?
+   */
   cacheRawField = global.cacheRawField || function(fieldNum, field) {
     //return true;
     return fieldNum !== 6 && fieldNum !== 10;
   };
 
+  /**
+   *  Add an item to d.
+   */
   var dTableNum  = 0;
   var addToD = function(x) {
     if (d[dTableNum].length >= tooManyData) {
@@ -75,6 +103,15 @@
     d[dTableNum].push(x);
   };
 
+  /*-------------------------------------------------------------------------------------------------------
+   *
+   * Generally, one-time functions are above, and utilities are below
+   *
+   *------------------------------------------------------------------------------------------------------- */
+
+  /**
+   *  Remove empty items from d.
+   */
   compactData = function() {
     var i, j, j2, oldColumn, record;
 
@@ -94,6 +131,67 @@
     setTimeout(function() {
       global.gc();
     }, 750);
+  };
+
+  /**
+   *  Send all records to the function (fn); then call fnAfterColumn after each column
+   */
+  eachRecord = function(fn, fnAfterColumn_, storeResult_) {
+    var fnAfterColumn = fnAfterColumn_ || function() {};
+    var storeResult   = storeResult_ || function() {};
+
+    // The inner loops get run bazillions of times, don't re-allocate variables.
+    var i, j, l, column, record;
+
+    // Loop over the columns (i), and the records (j)
+    for (i = 0; i < d.length; i++) {
+      column = d[i];
+      for (j = 0, l = column.length; j < l; j++) {
+        if (column.hasOwnProperty(j)) {
+          record = column[j];
+          //fn(record, j, column, i);
+          storeResult(fn(record, j, column, i));
+        }
+      }
+
+      fnAfterColumn(i, column);
+    }
+  };
+
+  /**
+   *  Like _.filter, when the fn returns truthy, add to collection.
+   */
+  filterIndex = function(name, fn) {
+    index[name] = index[name] || [];
+
+    eachRecord(function(record, j, column, i) {
+      if (fn.apply(this, arguments)) {
+        addToIndex(name, [i, j]);
+      }
+    });
+  };
+
+  /**
+   *  Add item(s) to an index
+   */
+  addToIndex = function(name /*, locs*/) {
+    var locs = _.rest(arguments);
+
+    if (locs.length === 1 && _.isArray(locs[0])) {
+      locs = locs[0];
+    }
+
+    index[name] = index[name] || [];
+    _.each(locs, function(loc) {
+      addToIndex(name, loc);
+    });
+  };
+
+  /**
+   *  Add one item to an index
+   */
+  addToIndex = function(name, loc) {
+    index[name].push(loc);
   };
 
 }());
